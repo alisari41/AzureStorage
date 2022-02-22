829e273f6822ccd097c40db6f1901a2612a79579
alisari41 <alisari41@outlook.com>
2022-02-23 00:41:43 +0300

    INoSqlStorage interface'i yazıldı. Bir tablo ile ilgili en sık kullanılan metodlar tanımlanıcak

---
+++ AzureStorageLibrary/INoSqlStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary
+{
+    interface INoSqlStorage<TEntity>
+    {//Bir tablo ile ilgili en sık kullanılan metodlar tanımlanıcak
+        Task<TEntity> Add(TEntity entity);
+        Task Delete(string rowKey, string partitionKey);
+        Task<TEntity> Update(TEntity entity);
+        Task<TEntity> Get(string rowKey, string partitionKey);//En verimli iki alanda arama yapıcaz
+        Task<IQueryable<TEntity>> All();
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> query);//Example : productTable.query(x=>x.price>100)
+    }
+}
---
+++ AzureStorageLibrary/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Product : TableEntity
+    {//Table Storage da tutulacak olan Entity yani satırım
+        // 4 tane sutun buradan gelicek 3 tanede default olarak gelicek
+        // RowKey = Birincil anahtar gibi
+        // PartitionKey = Satır Gruplama işlemi yaparak daha hızlı listelenmesini sağlar.Mesela kullanıcı şehirlerini tutuyoruz şehirleri gruplayabiliriz.
+        // TimeStamp = Zaman dalgası
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Color { get; set; }
+    }
+}
