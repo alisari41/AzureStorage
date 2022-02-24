9566bdbb032fa66ae64ba1eec6863bc20546d06e
alisari41 <alisari41@outlook.com>
2022-02-24 18:04:00 +0300

    BlobsController da Upload operasyonu tamamlandı. Resim ekleme işlemi tamamlandı

--- MvcWebApp/Controllers/BlobsController.cs
+++ MvcWebApp/Controllers/BlobsController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AzureStorageLibrary;
+using Microsoft.AspNetCore.Http;
 using MvcWebApp.Models;
 
 namespace MvcWebApp.Controllers
@@ -29,10 +31,23 @@ namespace MvcWebApp.Controllers
             {
                 Name = x,
                 //{blobUrl} /{x=Blob'ın ismi "png mi jpeg mi neyse o "}
-                Url = $"{blobUrl}/x"
+                Url = $"{blobUrl}/{x}"
             }).ToList();
 
-            return View(); 
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile picture)
+        {
+            //Eklenecek resim ismini random oluşturma. = Sol tarafda random isim + sağ tarafta uzantısı jpeg mi pbg mi falan
+            var newFileName = Guid.NewGuid().ToString() + Path.GetExtension(picture.FileName);
+
+
+            await _blobStorage.UploadAsync(picture.OpenReadStream(), newFileName, EContainerName.pictures);
+
+            return RedirectToAction("Index");
         }
     }
 }
