fe6e579d7650d8c18c644e42f3e6ab09a31af74a
alisari41 <alisari41@outlook.com>
2022-02-24 14:57:45 +0300

    IBlobStorage interfacesi oluşturuldu. Blob Storage’lar dosya kaydetmek için kullanılan servislerdir.

---
+++ AzureStorageLibrary/IBlobStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary
+{
+    public enum EContainerName
+    {//Resimleri ve pdf ler tutmak için container tutuyorum istersek video falan diğer uzantılar için devam edebiliriz
+        Pictures,
+        Pdf,
+        Logs,
+    }
+    public interface IBlobStorage
+    {
+        public string BlobUrl { get; set; }
+        //Task geriye herhangi birşey döndürmeyecek bu bizim Void metodlara karşılık gelir
+        Task UploadAsync(Stream fileStream, string fileName, EContainerName eContainerName);
+
+        //Task<Strean> geriye Stream döndürecek
+        Task<Stream> DownloadAsync(string fileName, EContainerName eContainerName);
+
+        Task DeleteAsync(string fileName);
+
+
+        //Block Blob
+        Task SetLogAsync(string text, string fileName);
+        Task<List<string>> GetLogAsync(string fileName); 
+
+
+
+        //Bir Container içindeki tüm blobları alma işlemi
+        List<string> GetNames(EContainerName eContainerName);
+    }
+}
